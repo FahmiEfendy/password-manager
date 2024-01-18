e13f81e16e81fa9dbc1ff57fa89c977dd5f65e07
Fahmi <itsfahmiefendy@gmail.com>
2024-01-18 13:34:49 +0700

    refactor: create password list component

---
+++ src/components/PasswordList/index.jsx
@@ -0,0 +1,85 @@
+import { useNavigate } from "react-router-dom";
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+
+import classes from "./style.module.scss";
+
+const PasswordList = ({ datas, setDeleteId, setIsDeleteModalOpen }) => {
+  const navigate = useNavigate();
+
+  const detailAccountHandler = (id) => {
+    navigate(`/detail/${id}`);
+  };
+
+  const deleteAccountHandler = (id) => {
+    setDeleteId(id);
+    setIsDeleteModalOpen(true);
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              <Typography variant="h5">Website</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography variant="h5">Email</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography variant="h5">Category</Typography>
+            </TableCell>
+            <TableCell align="center">
+              <Typography variant="h5">Action</Typography>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {datas.map((data) => {
+            return (
+              <TableRow key={data.id}>
+                <TableCell>
+                  <Typography variant="body1">{data.provider}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="body1">{data.email}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="body1">{data.category}</Typography>
+                </TableCell>
+                <TableCell className={classes.btn_container}>
+                  <Button
+                    variant="contained"
+                    onClick={() => detailAccountHandler(data.id)}
+                  >
+                    Detail
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="error"
+                    className={classes.btn_delete}
+                    onClick={() => deleteAccountHandler(data.id)}
+                  >
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default PasswordList;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,42 +1,18 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import {
-  Box,
-  Button,
-  CircularProgress,
-  Container,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from "@mui/material";
+import { Box, Button, CircularProgress, Container } from "@mui/material";
 
 import classes from "./style.module.scss";
 import { callApi } from "../../domain/api";
 import AddModal from "../../components/AddModal";
 import DeleteModal from "../../components/DeleteModal";
+import PasswordList from "../../components/PasswordList";
 
 const Home = () => {
-  const navigate = useNavigate();
-
   const [deleteId, setDeleteId] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [accountList, setAccountList] = useState([]);
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-
-  const detailAccountHandler = (id) => {
-    navigate(`/detail/${id}`);
-  };
-
-  const deleteAccountHandler = (id) => {
-    setDeleteId(id);
-    setIsDeleteModalOpen(true);
-  };
 
   const addPasswordHandler = () => {
     setIsAddModalOpen(true);
@@ -69,63 +45,14 @@ const Home = () => {
         </Button>
       </Box>
       {!isLoading ? (
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>
-                  <Typography variant="h5">Website</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="h5">Email</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="h5">Category</Typography>
-                </TableCell>
-                <TableCell align="center">
-                  <Typography variant="h5">Action</Typography>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {accountList.map((data) => {
-                return (
-                  <TableRow key={data.id}>
-                    <TableCell>
-                      <Typography variant="body1">{data.provider}</Typography>
-                    </TableCell>
-                    <TableCell>
-                      <Typography variant="body1">{data.email}</Typography>
-                    </TableCell>
-                    <TableCell>
-                      <Typography variant="body1">{data.category}</Typography>
-                    </TableCell>
-                    <TableCell className={classes.btn_container}>
-                      <Button
-                        variant="contained"
-                        onClick={() => detailAccountHandler(data.id)}
-                      >
-                        Detail
-                      </Button>
-                      <Button
-                        variant="contained"
-                        color="error"
-                        className={classes.btn_delete}
-                        onClick={() => deleteAccountHandler(data.id)}
-                      >
-                        Delete
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <PasswordList
+          datas={accountList}
+          setDeleteId={setDeleteId}
+          setIsDeleteModalOpen={setIsDeleteModalOpen}
+        />
       ) : (
         <CircularProgress />
       )}
-
       <DeleteModal
         id={deleteId}
         isOpen={isDeleteModalOpen}
